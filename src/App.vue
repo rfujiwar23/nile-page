cd5d475cab241f30dcb29a25a9e9f93bc7ec15ae
Ryo Fujiwara <jjcproject@icloud.com>
2021-09-06 09:49:34 +0900

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/nile-logo.png">
-  <Main msg="In Using Nile"/>
+  <div class="top">
+    <Navbar />
+    <div class="logo col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-8 offset-sm-2 col-10">
+      <img alt="Vue logo" src="./assets/nile-logo.png" class="img-fluid">
+    </div>
+    <Main class="text-center" msg="Online Business Exhibition, All At Your Fingertips"/>
+  </div>
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
+    Navbar,
     Main
   }
 }
@@ -19,8 +26,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+}
+
+.top {
+  background: url('assets/top-bg1.png');
+  background-size:cover;
+  background-position: center;
+  aspect-ratio: 32/9;
+}
+
+.logo {
+  margin:0 auto;
 }
 </style>
